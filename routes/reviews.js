3940c305b48641ef0a18b97fae39435692853693
aaronrosenzweig24 <aaronrosenzweig124@gmail.com>
2021-07-03 15:27:38 -0400

    restructure reviews route

---
+++ routes/reviews.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+const Review = require('../models/review');
+const Trail = require('../models/trails');
+const catchAsync = require('../utils/catchAsync');
+const { reviewSchema } = require('../schemas.js')
+const ExpressError = require('../utils/ExpressError');
+
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    } else {
+        next()
+    }
+}
+
+
+router.post('/', validateReview, catchAsync(async(req, res) => {
+    const trail = await Trail.findById(req.params.id);
+    const review = new Review(req.body.review);
+    trail.reviews.push(review);
+    await review.save();
+    await trail.save();
+    res.redirect(`/trails/${trail._id}`);
+}))
+
+router.delete('/:reviewId', catchAsync(async(req, res) => {
+    const { id, reviewId } = req.params;
+    await Trail.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/trails/${id}`);
+}))
+
+module.exports = router;
